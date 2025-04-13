b430bf267d69848049bc06c333371f628bc56e3f
bddeveloperabdullah <bddeveloperabdullah@gmail.com>
2025-04-13 23:45:03 +0600

    Booking area update

--- src/components/Home/Booking.jsx
+++ src/components/Home/Booking.jsx
@@ -12,43 +12,51 @@ const Booking = () => {
           <div>
             <form className="grid grid-cols-2 md:grid-cols-5">
               <div className="flex items-center gap-1.5 border-r border-[#eee] py-5 px-2.5">
-                <FaUserAlt className="text-secondary" />
+                <div>
+                  <FaUserAlt className="text-secondary" />
+                </div>
                 <input
                   type="text"
                   placeholder="Name"
-                  className=" px-2.5 focus:outline-none"
+                  className="text-xs md:text-base px-2.5 focus:outline-none"
                 />
               </div>
 
               <div className="flex items-center gap-1.5 border-r border-[#eee] py-5 px-2.5">
-                <HiOutlineMail className="text-secondary text-xl" />
+                <div>
+                  <HiOutlineMail className="text-secondary text-xl" />
+                </div>
                 <input
                   type="text"
                   placeholder="Email Address"
-                  className=" px-2.5 focus:outline-none"
+                  className="text-xs md:text-base px-2.5 focus:outline-none"
                 />
               </div>
 
               <div className="flex items-center gap-1.5 border-r border-[#eee] py-5 px-2.5">
-                <MdOutlineDateRange className="text-secondary text-xl" />
+                <div>
+                  <MdOutlineDateRange className="text-secondary text-xl" />
+                </div>
                 <input
                   type="text"
                   placeholder="Date"
-                  className=" px-2.5 focus:outline-none"
+                  className="text-xs md:text-base px-2.5 focus:outline-none"
                 />
               </div>
 
               <div className="flex items-center gap-1.5 border-r border-[#eee] py-5 px-2.5">
-                <IoMdTime className="text-secondary text-xl" />
+                <div>
+                  <IoMdTime className="text-secondary text-xl" />
+                </div>
                 <input
                   type="text"
                   placeholder="Time"
-                  className=" px-2.5 focus:outline-none"
+                  className="text-xs md:text-base px-2.5 focus:outline-none"
                 />
               </div>
 
               <div className="col-span-2 md:col-span-1">
-                <button className="w-full h-full py-4 md:py-0 hover:bg-black hover:text-white cursor-pointer transition-all duration-500">
+                <button className="w-full h-full py-4 md:py-0  hover:bg-black hover:text-white cursor-pointer transition-all duration-500">
                   MAKE A RESERVATION
                 </button>
               </div>
